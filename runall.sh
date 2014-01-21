5236a0dfec6ead370d7c8db8571c06c27246c4b7
Gabriel Wicke <wicke@wikidev.net>
2014-01-21 15:02:18 -0800

    Add HHVM benchmark and -results

--- runall.sh
+++ runall.sh
@@ -151,6 +151,156 @@ done
 
 cd ..
 
+echo ">>>>>>>>>> MediaWiki Templates (HHVM) <<<<<<<<<<<<<"
+cd mediawiki
+echo "*** test1 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test1.php
+done
+
+echo "*** test1b (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test1b-incid.php
+done
+
+echo "*** test2 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test2-loop.php
+done
+
+echo "*** test3 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test3-itterator.php
+done
+
+cd ..
+
+
+echo ">>>>>>>>>> Twig String No Cache (HHVM) <<<<<<<<<<<<<"
+cd twignocache
+echo "*** test1 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test1.php
+done
+
+echo "*** test1b (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test1b-incid.php
+done
+
+echo "*** test2 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test2-loop.php
+done
+
+echo "*** test3 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test3-itterator.php
+done
+
+cd ..
+
+
+echo ">>>>>>>>>> Twig File No Cache (HHVM) <<<<<<<<<<<<<"
+cd twignocache_file
+echo "*** test1 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test1.php
+done
+
+echo "*** test1b (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test1b-incid.php
+done
+
+echo "*** test2 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test2-loop.php
+done
+
+echo "*** test3 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test3-itterator.php
+done
+
+cd ..
+
+
+echo ">>>>>>>>>> Twig File Cached (HHVM) <<<<<<<<<<<<<"
+cd twigcache_file
+echo "*** test1 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test1.php
+done
+
+echo "*** test1b (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test1b-incid.php
+done
+
+echo "*** test2 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test2-loop.php
+done
+
+echo "*** test3 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test3-itterator.php
+done
+
+cd ..
+
+
+echo ">>>>>>>>>> Mustache (HHVM) <<<<<<<<<<<<<"
+cd mustache
+echo "*** test1 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test1.php
+done
+
+echo "*** test1b (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test1b-incid.php
+done
+
+echo "*** test2 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test2-loop.php
+done
+
+echo "*** test2 lambda (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test2-loop-lambda.php
+done
+
+echo "*** test3 (`pwd`)***"
+for i in {1..10}
+do
+	hhvm test3-itterator.php
+done
+
+cd ..
+
 echo ">>>>>>>>>> Mustache Node.js <<<<<<<<<<<<<"
 cd mustache-node
 echo "*** test1 (`pwd`)***"
